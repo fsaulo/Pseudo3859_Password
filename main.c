8865e1d3ede0513b39544c115c4987ed00171fe8
fsaulo <meetnapoleon@protonmail.ch>
2020-11-13 15:02:36 -0300

    Add useless and overcrowdded documentation as requested by the professor

--- main.c
+++ main.c
@@ -24,7 +24,6 @@
  *                                                                             *
  * PORTD.RD4:RD7    -> DISPLAY1:3                                              *
  * PORTD.RD0        -> POWER_LED                                               *
- * 
  ******************************************************************************/
 
 // Bits de configuracao
@@ -93,13 +92,12 @@ int main(void)
     uint8_t pit   = 200;          // Iteracoes de ociosidade
     uint8_t ccall = 0;            // Contador de iteracoes
     uint8_t preg  = 0;            // Apontador de registro
-    uint8_t nreg  = 4;            // Apontador de registro
+    uint8_t nreg  = 0;            // Apontador de registro
     uint8_t flag  = 1;            // Registra atuacao de botao 
        
     // Registrador onde sao armazenados os digitos fornecidos pelo usuario
-    // uint8_t  bus_register[4] = {0x0A, 0x0A, 0x0A, 0x0A};
-    uint8_t  bus_register[4] = {0x09, 0x05, 0x08, 0x03};
-            
+    uint8_t  bus_register[4] = {0x0A, 0x0A, 0x0A, 0x0A};
+    
     for (;;) {
         // Inicia displays
         bcd_output(bus_register, ccall, preg, pit);
@@ -142,6 +140,8 @@ void chk_passwd(uint8_t* breg, uint8_t* preg) {
     pass : if (OP_MODE) *preg = 0;
 }
 
+// Preenche o registrador com o valor 0xFF, codigo que diz que a mensagem foi 
+// inserida corretamente
 void disp_success(uint8_t* breg)
 {
     uint8_t i;
@@ -153,17 +153,21 @@ void disp_success(uint8_t* breg)
     }
 }
 
+// Preenche o registrador com o valor 0xF7, codigo que diz que a mensagem foi 
+// inserida de maneira incorreta
 void disp_error(uint8_t* breg)
 {
     uint8_t i;
    
     for (i = 0; i < 4; ++i)
     {
-        *breg = 0x00;
+        *breg = 0xF7;
         breg++;
     }   
 }
 
+// Incrementa o valor atual do buffer, se o valor ultrapassar 9, o valor do
+// registrador volta para 0
 void key_fwd(uint8_t* bus, uint8_t pos)
 {
     if (bus[pos] >= 9) {
@@ -173,6 +177,7 @@ void key_fwd(uint8_t* bus, uint8_t pos)
     }
 }
 
+// Preenche o registrador com o valor 0x0A, codigo que representa sem conteudo
 void clearreg(uint8_t* breg)
 {
     uint8_t i;
@@ -183,9 +188,19 @@ void clearreg(uint8_t* breg)
     }
 }
 
-// Lida com entradas e saidas
+/*
+ * Lida com todas as entradas e algumas saidas do sistema.
+ * Possui mecanismo de deteccao de falso acionamento de botao ou sistema de 
+ * deboucing.
+ * @args: bus  -> buffer atual
+ *        preg -> apontador para a posicao no buffer
+ *        nreg -> apontador especial
+ *        flag -> sinaliza botao pressionado e bloqueia outras entradas
+ */
 void iohandler(uint8_t* bus, uint8_t* preg, uint8_t* nreg, uint8_t* flag)
 {
+    
+    // Incrementa o conteudo do buffer na posicao indicado por preg
     if (SW2 == 1U && *flag == 1) {
         __delay_us(50);
         if (SW2 == 1) {
@@ -194,6 +209,8 @@ void iohandler(uint8_t* bus, uint8_t* preg, uint8_t* nreg, uint8_t* flag)
         }
     }    
     
+    // Limpa o conteudo do buffer e reseta as posicoes em preg e nreg
+    // fazendo o sistema voltar ao estado inicial
     if (SW3 == 1U && *flag == 1) {
         __delay_us(50);
         if (SW3 == 1) {
@@ -204,16 +221,22 @@ void iohandler(uint8_t* bus, uint8_t* preg, uint8_t* nreg, uint8_t* flag)
         }
     }
     
+    // Desbloqueia o registrador de entradas para receber novos comandos caso
+    // o botao tenha sido pressionado e depois solto
     if (SW0 == 0 && SW1 == 0 && SW2 == 0  && SW3 == 0 && *flag == 0) {
         *flag = 1U;
     }
     
-    uint8_t pp;
+    uint8_t pp; // variavel auxiliar
     
+    // Verifica se esta atualmente recebendo comandos do tipo avancado ou
+    // comandos simplificados
     switch ( OP_MODE ) {
+        
+        // Modo simplificado: os numeros se deslocam da direita para a esquerda
+        // a medida que forem sendo inseridos
         case 0:
-            if (SW0 == 1U && *flag == 1) {
-                
+            if (SW0 == 1U && *flag == 1) {      
                 __delay_us(50);
                 if (SW0 == 1) {
                     
@@ -227,6 +250,9 @@ void iohandler(uint8_t* bus, uint8_t* preg, uint8_t* nreg, uint8_t* flag)
                 }
             }
             break;
+            
+        // Modo avancado: movimenta um cursor no qual eh possivel incrementar o
+        // valor da posicao do cursor
         case 1:
             if (SW0 == 1U && *flag == 1) {
                 __delay_us(50);
@@ -270,25 +296,39 @@ void bcd_output(uint8_t* value, uint8_t cwait, uint8_t preg, uint8_t pit)
     uint8_t pdisp;
     uint16_t sbus;
     uint8_t func = *value;
+    
+    // Armazena os valores em hexadecimal que representam caracteres em bcd
+    // num_bcd_7seg  -> numerico bcd de 0-9
+    // num_pass_7seg -> alfa numerico bcd escrevem a palavra PIC16F887
+    // num_erro_7seg -> alfa numerico bcd escrevem a palavra ErrO
+    // aux_bus       -> apaga todos os leds do display
     uint8_t num_bcd_7seg[]  = { 0x01, 0x4F, 0x12, 0x06, 0x4C, 0x24, 0x20, 0x0F, 
-                                0x00, 0x04, 0xFE };
+                                0x00, 0x04, 0xFE, 0x7E };
     uint8_t num_pass_7seg[] = { 0x18, 0x4F, 0x31, 0x4F, 0x20, 0x38, 0x00, 0x00,
-                                0x0F, 0x7F, 0x7F, 0x7F};
-    uint8_t num_erro_7seg[] = { 0x30, 0x7A, 0x7A, 0x01};
-    uint8_t aux_bus[4]      = {0x18, 0x7F, 0x7F, 0x7F};
-    uint8_t t;
-    uint8_t pr = 0, li = 0;
+                                0x0F, 0x7F, 0x7F, 0x7F };
+    uint8_t num_erro_7seg[] = { 0x01, 0x7A, 0x7A, 0x30 };
+    uint8_t aux_bus[4]      = { 0x18, 0x7F, 0x7F, 0x7F };
+    
+    uint8_t t;              // Variavel auxiliar
+    uint8_t pr = 0;         // Varialve auxiliar
+    uint8_t li = 0;         // Variavel auxiliar
     
     // Bloco responsavel pela multiplexacao dos displays de 7 segmentos.
     // As saidas sao configuradas para intercambiar entre regioes de corte
     // e saturacao de 4 TBJs tipo PNP com funcao de ativar e desativar o
     // anodo comum de cada display com periodo de 500 us.
-    
     switch(func) {
+        
+        // Envia para os displays a palavra PIC16F887, realiza a multiplexacao
+        // entre os displays e desloca cada letra para direta
         case 0xFF:
-            for (sbus = 0; sbus < 100*12; ++sbus) {
+            for (sbus = 0; sbus < 50*12; ++sbus) {
                 if (SW3 == 1) break;
-                if (pr > 100) {
+                
+                // Conta 50 periodos de 800 us e desloca o conteudo da bus auxi-
+                // liar para esquerda. A primeira posicao da bus recebe uma le-
+                // tra da palavra PIC16F887 de acordo com o apontador de posicao
+                if (pr > 50) {
                     li++;
                     if (li >= 12) li = 0;
                     pr = 0;
@@ -298,18 +338,48 @@ void bcd_output(uint8_t* value, uint8_t cwait, uint8_t preg, uint8_t pit)
                     }
                     aux_bus[0] = num_pass_7seg[li];
                 }
-
+                  
+                // Responsavel por intercalar entre os displays
                 for (pdisp = 0; pdisp < 4; ++pdisp) {
                     PORTB = aux_bus[pdisp];
                     mux_display(pdisp);
-                    __delay_us(500);
+                    __delay_us(800);
                 }
                 pr++;
             }
 
             break;
+            
+        // Envia para os displays a palavra ErrO, realiza a multplexacao entre
+        // os displays e pisca cada letra em intervalos de aproximadamente 500ms
         case 0xF7:
+            
+            for (li = 0; li < 2; ++li) {
+                
+                // Responsavel pela multiplexacao dos displays
+                for (pdisp = 0; pdisp < 4; ++pdisp) {
+                    mux_display(pdisp);
+                    PORTB = num_erro_7seg[pdisp];
+                    sbus++;
+                    __delay_us(500); 
+
+                }
+                
+                // Conta 500 periodos de aproximadamente 500 us e desliga todos
+                // os leds por 500 ms
+                if (sbus > 500) {
+                    PORTB = 0x7F;
+                    cdisplay();
+                    __delay_ms(500);
+                    sbus = 0;
+                }
+            }
+            
             break;
+        
+        // Envia numeros de 0-9 para os displays de acordo com o valor que rece-
+        // be do buffer. Esse valor determina a posicao a ser acessada do vetor
+        // 'num_bcd_7seg', que contem os valores em bcd
         default:
             for (pdisp = 0; pdisp < 4; ++pdisp) {
         
@@ -335,6 +405,8 @@ void bcd_output(uint8_t* value, uint8_t cwait, uint8_t preg, uint8_t pit)
     }
 }
 
+// Bloco responsavel por intercalar entre os displays, o display atualmente li-
+// gado e determiado pelo apontador 'pdisp'
 void mux_display(uint8_t pdisp) {
     switch ( pdisp ) {
         case 0:
